3b7f9e9263b65aa68f98fe49226ca300fd3f627b
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-09 23:45:09 +0300

    [web] moved files to root directory

--- mobile/App.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import Layout from './src/app/layout/layout'
-
-export default function App() {
-  return <Layout />
-}
-
-const styles = StyleSheet.create({})
